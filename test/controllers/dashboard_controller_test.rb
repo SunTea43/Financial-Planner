642a40d72174ca262f64e72d2995983c77c644f1
Santiago Pérez <santipego0001@gmail.com>
2026-01-24 11:21:01 -0500

    test: fix defaults test for CI

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index when authenticated" do
+    sign_in @user
     get dashboard_index_url
     assert_response :success
+  end
+
+  test "should redirect to sign in when not authenticated" do
+    get dashboard_index_url
+    assert_redirected_to new_user_session_path
   end
 end
