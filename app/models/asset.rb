e0c3fc3535afa9a8dcaff191b3780e4fecfcf4f3
Santiago Pérez <santipego0001@gmail.com>
2026-03-07 17:26:03 -0500

    docs: makes annotations for existing models

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: balance_sheet_items
+#
+#  id               :bigint           not null, primary key
+#  balance_sheet_id :bigint           not null
+#  name             :string           not null
+#  item_type        :string           not null
+#  category         :string
+#  amount           :decimal(15, 2)   not null
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  position         :integer
+#  type             :string
+#
+# Indexes
+#
+#  index_balance_sheet_items_on_balance_sheet_id                (balance_sheet_id)
+#  index_balance_sheet_items_on_balance_sheet_id_and_item_type  (balance_sheet_id,item_type)
+#  index_balance_sheet_items_on_category                        (category)
+#
 class Asset < BalanceSheetItem
   ASSET_TYPES = %w[liquid fixed].freeze
   CATEGORIES = {
