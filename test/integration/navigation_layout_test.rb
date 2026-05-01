a33861d3efa2aeb1af5794be07f08a2282b344b6
Santiago Pérez <santipego0001@gmail.com>
2026-05-01 18:02:21 -0500

    feat: add collapsible sidebar toggle in navbar

--- test/integration/navigation_layout_test.rb
+++ test/integration/navigation_layout_test.rb
@@ -11,13 +11,16 @@ class NavigationLayoutTest < ActionDispatch::IntegrationTest
     get dashboard_index_url
     assert_response :success
 
-    assert_select "aside .sidebar-nav-card", count: 1
-    assert_select "aside .list-group a[href='#{authenticated_root_path}']", count: 1
-    assert_select "aside .list-group a[href='#{accounts_path}']", count: 1
-    assert_select "aside .list-group a[href='#{balance_sheets_path}']", count: 1
-    assert_select "aside .list-group a[href='#{budgets_path}']", count: 1
-    assert_select "aside .list-group a[href='#{savings_plans_path}']", count: 1
-    assert_select "aside .list-group a[href='#{reports_path}']", count: 1
+    assert_select "#sidebarNav.collapse.show.sidebar-nav", count: 1
+    assert_select "#sidebarNav .sidebar-nav-card", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{authenticated_root_path}']", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{accounts_path}']", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{balance_sheets_path}']", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{budgets_path}']", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{savings_plans_path}']", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{reports_path}']", count: 1
+
+    assert_select "nav button[data-bs-toggle='collapse'][data-bs-target='#sidebarNav'][aria-controls='sidebarNav']", count: 1
 
     assert_select "nav .dropdown-menu a[href='#{edit_user_registration_path}']", count: 1
     assert_select "nav .dropdown-menu a[href='#{data_export_path}']", count: 1
@@ -36,15 +39,16 @@ class NavigationLayoutTest < ActionDispatch::IntegrationTest
     get budgets_path
     assert_response :success
 
-    assert_select "aside .list-group a[href='#{budgets_path}'].active", count: 1
-    assert_select "aside .list-group a[href='#{accounts_path}'].active", count: 0
+    assert_select "#sidebarNav .list-group a[href='#{budgets_path}'].active", count: 1
+    assert_select "#sidebarNav .list-group a[href='#{accounts_path}'].active", count: 0
   end
 
   test "guests do not see sidebar or authenticated navbar" do
     get root_path
     assert_response :success
 
-    assert_select "aside .sidebar-nav-card", count: 0
+    assert_select "#sidebarNav", count: 0
+    assert_select "nav button[data-bs-target='#sidebarNav']", count: 0
     assert_select "nav .dropdown-menu", count: 0
   end
 end
