6521b5de54d730c1c1a647c8f0c2ab3f70a14b7b
Santiago Pérez <santipego0001@gmail.com>
2026-05-01 16:01:52 -0500

    refactor: use enum scope methods in ShowComponent instead of string comparisons
    Replace manual select { |x| x.item_type == "..." } with the enum
    scopes generated by Rails: assets.liquid, assets.fixed,
    liabilities.short_term, liabilities.long_term

--- app/components/balance_sheets/show_component.rb
+++ app/components/balance_sheets/show_component.rb
@@ -10,18 +10,18 @@ class BalanceSheets::ShowComponent < ApplicationComponent
   end
 
   def liquid_assets
-    balance_sheet.assets.select { |a| a.item_type == "liquid" }
+    balance_sheet.assets.liquid
   end
 
   def fixed_assets
-    balance_sheet.assets.select { |a| a.item_type == "fixed" }
+    balance_sheet.assets.fixed
   end
 
   def short_term_liabilities
-    balance_sheet.liabilities.select { |l| l.item_type == "short_term" }
+    balance_sheet.liabilities.short_term
   end
 
   def long_term_liabilities
-    balance_sheet.liabilities.select { |l| l.item_type == "long_term" }
+    balance_sheet.liabilities.long_term
   end
 end
